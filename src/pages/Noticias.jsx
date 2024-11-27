fce23b90c7dc5b4db00995702adadd261d195ad1
Gustavo Creado Figueroa <gcreado@cellit.cl>
2024-11-27 17:12:31 -0300

    Se actualizan cambios para responsive

--- src/pages/Noticias.jsx
+++ src/pages/Noticias.jsx
@@ -41,114 +41,80 @@ const Noticias = () => {
           </div>
 
           <div className="row g-0">
-            <div className="col-12 col-lg-6 mb-4 mt-4" data-aos="fade-left">
-              <div className="row">
-                <div className="card bg-gradient-card-noticias shadow-lg">
+            <div className="container">
+              <div className="row g-0">
+                <div className="col-12 col-lg-6 mb-4 mt-4" data-aos="fade-left">
                   <div className="row">
-                    <div className="col-4 position-relative">
-                      <img
-                        className="position-absolute top-50 start-50 translate-middle rounded shadow-lg"
-                        src="./portada2.jpg"
-                        style={{ height: "250px", width: "200px" }}
-                        alt=""
-                      />
-                    </div>
-
-                    <div className="col-8">
-                      <div className="card-body text-center mx-2">
-                        <div className="text-center mb-4">
-                          <h4 className="text-white">
-                            Servicios
-                            <span className="fw-bold fst-italic text-white">
-                              {" "}
-                              Contables
-                            </span>
-                          </h4>
-                          <br />
-                          <p className="card-text text-light">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing
-                            elit. Natus magnam quia nesciunt.
-                          </p>
+                    <div className="card bg-gradient-card-noticias shadow-lg">
+                      <div className="row">
+                        <div className="col-4 position-relative">
+                          <img
+                            className="position-absolute top-50 start-50 translate-middle rounded shadow-lg"
+                            src="./portada2.jpg"
+                            style={{ height: "250px", width: "200px" }}
+                            alt=""
+                          />
                         </div>
-                        <a href="#" className="btn btn-md btn-light">
-                          Leer mas
-                        </a>
+
+                        <div className="col-8">
+                          <div className="card-body text-center mx-2">
+                            <div className="text-center mb-4">
+                              <h4 className="text-white">
+                                Servicios
+                                <span className="fw-bold fst-italic text-white">
+                                  {" "}
+                                  Contables
+                                </span>
+                              </h4>
+                              <br />
+                              <p className="card-text text-light">
+                                Lorem ipsum dolor sit amet consectetur,
+                                adipisicing elit. Natus magnam quia nesciunt.
+                              </p>
+                            </div>
+                            <a href="#" className="btn btn-md btn-light">
+                              Leer mas
+                            </a>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div className="col-12 col-lg-6 mb-4 mt-4" data-aos="fade-left">
-              <div className="row">
-                <div className="card bg-gradient-card-noticias shadow-lg">
+                <div className="col-12 col-lg-6 mb-4 mt-4" data-aos="fade-left">
                   <div className="row">
-                    <div className="col-4 position-relative">
-                      <img
-                        className="position-absolute top-50 start-50 translate-middle rounded shadow-lg"
-                        src="./portada2.jpg"
-                        style={{ height: "250px", width: "200px" }}
-                        alt=""
-                      />
-                    </div>
-
-                    <div className="col-8">
-                      <div className="card-body text-center mx-2">
-                        <div className="text-center mb-4">
-                          <h4 className="text-white">
-                            Servicios
-                            <span className="fw-bold fst-italic text-white">
-                              {" "}
-                              Contables
-                            </span>
-                          </h4>
-                          <br />
-                          <p className="card-text text-light">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing
-                            elit. Natus magnam quia nesciunt.
-                          </p>
+                    <div className="card bg-gradient-card-noticias shadow-lg">
+                      <div className="row">
+                        <div className="col-4 position-relative">
+                          <img
+                            className="position-absolute top-50 start-50 translate-middle rounded shadow-lg"
+                            src="./portada2.jpg"
+                            style={{ height: "250px", width: "200px" }}
+                            alt=""
+                          />
                         </div>
-                        <a href="#" className="btn btn-md btn-light">
-                          Leer mas
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-12 col-lg-6 mb-4 mt-4" data-aos="fade-left">
-              <div className="row">
-                <div className="card bg-gradient-card-noticias shadow-lg">
-                  <div className="row">
-                    <div className="col-4 position-relative">
-                      <img
-                        className="position-absolute top-50 start-50 translate-middle rounded shadow-lg"
-                        src="./portada2.jpg"
-                        style={{ height: "250px", width: "200px" }}
-                        alt=""
-                      />
-                    </div>
 
-                    <div className="col-8">
-                      <div className="card-body text-center mx-2">
-                        <div className="text-center mb-4">
-                          <h4 className="text-white">
-                            Servicios
-                            <span className="fw-bold fst-italic text-white">
-                              {" "}
-                              Contables
-                            </span>
-                          </h4>
-                          <br />
-                          <p className="card-text text-light">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing
-                            elit. Natus magnam quia nesciunt.
-                          </p>
+                        <div className="col-8">
+                          <div className="card-body text-center mx-2">
+                            <div className="text-center mb-4">
+                              <h4 className="text-white">
+                                Servicios
+                                <span className="fw-bold fst-italic text-white">
+                                  {" "}
+                                  Contables
+                                </span>
+                              </h4>
+                              <br />
+                              <p className="card-text text-light">
+                                Lorem ipsum dolor sit amet consectetur,
+                                adipisicing elit. Natus magnam quia nesciunt.
+                              </p>
+                            </div>
+                            <a href="#" className="btn btn-md btn-light">
+                              Leer mas
+                            </a>
+                          </div>
                         </div>
-                        <a href="#" className="btn btn-md btn-light">
-                          Leer mas
-                        </a>
                       </div>
                     </div>
                   </div>
