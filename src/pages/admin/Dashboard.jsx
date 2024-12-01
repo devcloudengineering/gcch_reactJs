e9949e09eb2c1f03f35457a86b5fd6bbaf0739bc
Gustavo Creado Figueroa <gcreado@cellit.cl>
2024-11-30 22:01:44 -0300

    Se actualiza componente admin principal

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -2,11 +2,9 @@ import { Route, Routes, NavLink } from "react-router-dom";
 import PrincipalAdmin from "./components/PrincipalAdmin";
 
 // Componentes de las secciones
-const Home = () => <h2>Inicio</h2>;
 const Entries = () => <h2>Entradas</h2>;
 const Users = () => <h2>Usuarios</h2>;
 const Settings = () => <h2>Configuración</h2>;
-const Logout = () => <h2>Salir</h2>;
 
 const Dashboard = () => {
   return (
@@ -14,8 +12,8 @@ const Dashboard = () => {
       <div className="row">
         {/* Barra lateral */}
         <aside className="barra-lateral col-12 col-sm-auto p-0">
-          <div className="logo">
-            <h2 className="py-4 m-0">Dashboard</h2>
+          <div className="logo bg-dark fst-italic">
+            <h2 className="py-4 m-0">Panel Admin</h2>
           </div>
           <nav className="menu d-flex d-sm-block justify-content-center flex-wrap">
             <NavLink to="" className="nav-link" end>
@@ -30,7 +28,7 @@ const Dashboard = () => {
             <NavLink to="settings" className="nav-link">
               Configuración
             </NavLink>
-            <NavLink to="logout" className="nav-link">
+            <NavLink to="/login" className="nav-link">
               Salir
             </NavLink>
           </nav>
@@ -43,7 +41,6 @@ const Dashboard = () => {
             <Route path="entries" element={<Entries />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
-            <Route path="logout" element={<Logout />} />
           </Routes>
         </main>
       </div>
