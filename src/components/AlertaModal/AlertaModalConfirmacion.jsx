84a6ed6e2e91112b50fad49d84c0bfb065d103aa
Gustavo Creado Figueroa <gcreado@cellit.cl>
2024-12-10 14:44:44 -0300

    Se añade modal de confirmacion para eliminar clientes

---
+++ src/components/AlertaModal/AlertaModalConfirmacion.jsx
@@ -0,0 +1,84 @@
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+
+const AlertaModalConfirmacion = ({ show, title, body, onClose, onConfirm }) => {
+  const [isVisible, setIsVisible] = useState(show);
+
+  useEffect(() => {
+    if (show) {
+      setIsVisible(true);
+    }
+  }, [show]);
+
+  const handleClose = () => {
+    setIsVisible(false);
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  };
+
+  const handleConfirm = () => {
+    onConfirm(); // Llamada para confirmar la acción
+    handleClose(); // Cierra el modal
+  };
+
+  return (
+    <div
+      className={`modal fade ${isVisible ? "show" : ""}`}
+      tabIndex="-1"
+      style={{
+        display: show || isVisible ? "block" : "none",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+      }}
+      role="dialog"
+      aria-hidden={!show}
+    >
+      <div
+        className={`modal-dialog text-center ${
+          isVisible ? "animate-in" : "animate-out"
+        }`}
+      >
+        <div className="modal-content bg-light">
+          <div className="modal-header">
+            <h5 className="modal-title">{title}</h5>
+            <button
+              type="button"
+              className="btn-close"
+              onClick={handleClose}
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <p>{body}</p>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-lg btn-secondary mx-auto"
+              onClick={handleClose}
+            >
+              Cancelar
+            </button>
+            <button
+              type="button"
+              className="btn btn-lg btn-danger bg-gradient mx-auto"
+              onClick={handleConfirm}
+            >
+              Confirmar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+AlertaModalConfirmacion.propTypes = {
+  show: PropTypes.bool.isRequired,
+  title: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+};
+
+export default AlertaModalConfirmacion;
--- src/pages/admin/components/Clientes.jsx
+++ src/pages/admin/components/Clientes.jsx
@@ -7,11 +7,16 @@ import {
   getSortedRowModel,
   getFilteredRowModel,
 } from "@tanstack/react-table";
+import AlertaModal from "../../../components/AlertaModal/AlertaModal";
 
 const Clientes = () => {
   const [data, setData] = useState(null); // Estado para almacenar los datos
   const [loading, setLoading] = useState(true); // Estado para manejar la carga
   const [error, setError] = useState(null); // Estado para manejar errores
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleCloseModal = () => {
+    setIsModalOpen(false); // Cierra el modal
+  };
 
   const columns = [
     {
@@ -72,15 +77,6 @@ const Clientes = () => {
       footer: "Domicilio",
     },
     {
-      header: "Notificaciones",
-      accessorKey: "notificaciones",
-      footer: "Notificaciones",
-      cell: (info) => {
-        const email = info.getValue();
-        return email ? email.toLowerCase() : ""; // Convierte el email a mayúsculas
-      },
-    },
-    {
       header: "Telefono",
       accessorKey: "telefono",
       footer: "Telefono",
@@ -101,7 +97,68 @@ const Clientes = () => {
           <span className="badge bg-danger">Inactivo </span>
         ),
     },
+    {
+      header: "Acciones",
+      accessorKey: "acciones",
+      cell: (info) => {
+        const cliente = info.row.original;
+
+        return (
+          <div className="d-flex gap-2">
+            <button
+              className="btn btn-warning btn-sm px-4 py-2 shadow-sm border-0 rounded-3 hover:bg-warning focus:ring-2 focus:ring-warning small"
+              onClick={() => handleEdit(cliente._id)}
+            >
+              Modificar
+            </button>
+            <button
+              className="btn btn-danger btn-sm px-4 py-2 shadow-sm border-0 rounded-3 hover:bg-danger focus:ring-2 focus:ring-danger small"
+              onClick={() => handleDelete(cliente._id)}
+            >
+              Eliminar
+            </button>
+          </div>
+        );
+      },
+    },
   ];
+
+  const handleEdit = (cliente) => {
+    // Lógica para modificar el cliente (abrir formulario de edición o algo similar)
+    console.log("Editar cliente", cliente);
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      // setIsModalOpenConfirmacion(true);
+      const token = localStorage.getItem("token"); // Obtener el token de localStorage (o de donde lo almacenes)
+      if (!token) {
+        throw new Error("Token no encontrado. Por favor, inicia sesión.");
+      }
+
+      const response = await fetch(`http://localhost:3000/api/clientes/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          token,
+        },
+      });
+
+      const result = await response.json();
+      const { clientes } = result;
+      console.log(response);
+      if (response.ok) {
+        // Elimina el cliente de la lista de datos
+        setData(clientes);
+        setIsModalOpen(true);
+      } else {
+        console.error(result);
+        throw new Error(`Error: ${result.msg}`);
+      }
+    } catch (error) {
+      alert(`Error al eliminar el cliente: ${error}`);
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -141,18 +198,33 @@ const Clientes = () => {
     onGlobalFilterChange: setFiltering,
   });
 
-  if (loading) return <p>Loading...</p>; // Mensaje de carga
+  if (loading) {
+    return (
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <div className="spinner-border text-primary" role="status"></div>
+      </div>
+    );
+  }
   if (error) return <p>Error: {error}</p>; // Mensaje de error
 
   return (
     <div className="table-responsive table-sm small">
-      <input
-        type="text"
-        value={filtering}
-        onChange={(e) => setFiltering(e.target.value)}
-        className="form-control "
-        placeholder="Buscar..."
-      />
+      <div className="input-group">
+        <span className="input-group-text" id="buscar-addon">
+          🔍
+        </span>
+        <input
+          type="text"
+          value={filtering}
+          onChange={(e) => setFiltering(e.target.value)}
+          className="form-control"
+          placeholder="Buscar..."
+          id="buscar"
+        />
+      </div>
       <div
         className="overflow-auto"
         style={{ maxHeight: "500px", minHeight: "500px" }}
@@ -282,6 +354,14 @@ const Clientes = () => {
               >
                 Última &raquo;
               </button>
+              {
+                <AlertaModal
+                  show={isModalOpen}
+                  title="Cliente eliminado exitosamente"
+                  body="El cliente ha sido eliminado de la base de datos"
+                  onClose={handleCloseModal}
+                />
+              }
             </li>
           </ul>
         </nav>
